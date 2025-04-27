94d099e865c5c6ec0d513ecf9d4cf152a652ca17
Chamijd <chamindumax28@gmail.com>
2025-04-27 13:55:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -198,7 +198,7 @@ const port = process.env.PORT || 9090;
   conn.sendMessage(from, { text: teks }, { quoted: mek })
   }
   const udp = botNumber.split('@')[0];
-    const jawad = (94783314361', '94772706360', '94774575878');
+    const jawad = ('94783314361', '94772706360', '94774575878');
     let isCreator = [udp, jawad, config.DEV]
 					.map(v => v.replace(/[^0-9]/g) + '@s.whatsapp.net')
 					.includes(mek.sender);
